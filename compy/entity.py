ebefd4787a6b63dbb9abe0696671a19478c219dc
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-10 06:48:22 +1000

    Update entity
    Add a data store (dictionary).
    Register entities with a central dictionary.
    Add a find_entity static method.
    Add an update_entities static method.
    Use the component.type instead of requiring it
    as a parameter.

--- compy/entity.py
+++ compy/entity.py
@@ -4,23 +4,52 @@ Created on 09/05/2012
 @author: adam
 '''
 
+import weakref
 
 class Entity( object ):
+    entities = {}
     
     def __init__( self, name ):
         super( Entity, self ).__init__()
 
         self.name = name
         self.components = {}
+        self.data = {}
 
-    def add_component( self, type, component ):
-        if type in self.components:
+        if name in Entity.entities:
+            raise ValueError( "Entity name must be unique" )
+        Entity.entities[ name ] = weakref.ref( self )
+
+    def __del__( self ):
+        del Entity.entities[ self.name ]
+
+    @staticmethod
+    def find_entity( name ):
+        if name in Entity.entities:
+            entity = Entity.entities[ name ]
+            if entity == None:
+                del Entity.entities[ name ]
+                return None
+            else:
+                return entity()
+        return None
+
+    @staticmethod
+    def update_entities():
+        for entity in Entity.entities.values():
+            if entity != None:
+                entity().update()
+            else:
+                del Entity.entities[ name ]
+
+    def add_component( self, component ):
+        if component.type in self.components:
             raise ValueError(
                 "Component type already registered"
                 )
         else:
             component._set_entity( self )
-            self.components[ type ] = component
+            self.components[ component.type ] = component
 
     def remove_component( self, type ):
         component = self.components[ type ]
